fba5c120b21c05d8a7d6dd5702ead3422a486b67
Michel de Leeuw <michel@micheldeleeuw.nl>
2026-08-19 10:28:06 +0200

    .

--- lion_tools/DataFrameDisplay.py
+++ lion_tools/DataFrameDisplay.py
@@ -743,7 +743,7 @@ class DataFrameDisplay():
                 (len(value) == 32 and all(c in '0123456789abcdefABCDEF' for c in value))
                     or
                 (len(value) == 36 and all(c in '0123456789abcdef-' for c in value))
-            ):
+            )):
             value = html.escape(value)
             return value[0:5] + "...." + value[-5:]
         else:
